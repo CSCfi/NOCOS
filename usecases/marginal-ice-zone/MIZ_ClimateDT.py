49a354ca992f33533b346eacb85496ca0fde99b6
Keguang Wang <keguang.wang@met.no>
2025-11-11 23:43:39 +0100

    refine MIZ_ClimateDT

--- usecases/marginal-ice-zone/MIZ_ClimateDT.py
+++ usecases/marginal-ice-zone/MIZ_ClimateDT.py
@@ -7,6 +7,7 @@
 
 """
 
+#=====================================================================
 import sys, os
 import numpy as np
 import pandas as pd
@@ -17,23 +18,29 @@ from read_ice_data import read_ice_data
 from utils.save_MIZ_toNetCDF import save_toNetcdf
 from utils.calculate_MIZ import calc_MIZ
 
+#=====================================================================
 dataSource = 'ClimateDT'
 
 # Read config file
 configs=read_configfile(dataSource)
 
+# set start and end years for the dataset
+year_start = int(configs['selectdata']['year_start'])
+year_end   = int(configs['selectdata']['year_end'])
+
+if year_end <= 2019:
+   simulationPeriod = 'historical'
+elif year_start >= 2020:
+   simulationPeriod = 'future'
+
+#=====================================================================
 # Read ice data
 for dataset in configs['selectdata']['datasets']:
-    print('Processing ' + dataset + ':')
 
-    if 'future' in dataset:
-       year_start, year_end = 2030, 2039
-    elif 'hist' in dataset:
-       year_start, year_end = 2010, 2019
-    else:
-       print('dataset not available, exit ...')
-       sys.exit()
+    print('Processing ' + dataset + ':')
     
+    dataset1 = dataset +'_' + simulationPeriod
+
     dates = []
     for year in range(year_start,year_end+1):
         for month in range(1,13):
@@ -46,9 +53,9 @@ for dataset in configs['selectdata']['datasets']:
             print('   processing date ' + str(year*100 + month) + ' ...')
             try:
                 configs['date'] = str(year*100 + month)
-                configs['dataset'] = dataset
-                configs['ice_filename'] = os.path.join(configs['ice_folder'],dataset,
-                   dataset.split('_')[0] + '_monthly_mean_' + configs['date'] + '.nc')
+                configs['dataset'] = dataset1
+                configs['ice_filename'] = os.path.join(configs['ice_folder'],dataset1,
+                   dataset + '_monthly_mean_' + configs['date'] + '.nc')
                 icedata=read_ice_data(configs)
                 
                 # Calculate MIZ
@@ -66,6 +73,6 @@ for dataset in configs['selectdata']['datasets']:
                 print('     No data in ' + str(year*100+month))
                 df.loc[str(100*year+month),['miz_ext', 'mean_lat']] = ['NaN', 'NaN']
             
-    df.to_csv(os.path.join(configs['output']['output_folder'], dataset+'_stats.txt'), 
+    df.to_csv(os.path.join(configs['output']['output_folder'], dataset1+'_stats.txt'), 
               sep='\t', index=True,header=False)
 
